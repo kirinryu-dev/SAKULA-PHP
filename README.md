6e180f80b858ef33b762641cc4431c4c307d8e2a
kirinryu.dev <kirinryu.dev@gmail.com>
2025-04-16 15:20:56 +0000

    sakula first edit

---
+++ README.md
@@ -0,0 +1,18 @@
+## This project is a simple form to add with solidity language 
+## 🎯 Objective: 
+- Create a basic task Submission Form save submited data into a task.jsaon file .
+- Display tasks below the form (optional)
+
+## Folder structure
+project-root/
+|
+|- assets/
+|- config/
+|- includes/
+|- php/
+|- index.php
+|- README.md
+
+## NB 
+- as a joke SAKULA here is solidity.
+
--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php 
 
-echo "Hello World! This is a test file.";
+?>
+
+<div>
+    <!-- the header   -->
+    <?php include_once 'includes/header.php' ;  ?>
+
+</div>
+
+<main>
+    <!-- the main content    -->
+    <?php include_once 'includes/navbar.php' ?>
+    <div>
+        <!-- this is where the OPS will happen   -->
+        <h2>SAKULA Form</h2>
+        <div class="form_container">
+            <form action="php/traitement.php" method="POST">
+                
+            </form>
+
+            <h2>
+                <!-- Tasks display    -->
+                 <span>
+                    Tasks Display  
+                 </span> 
+            </h2>
+            <div class="tasks_display-container">
+
+            </div>
+        </div>
+    </div>
+</main>
+
+<div>
+    <!-- the footer   -->
+   <?php  include_once 'includes/footer.php'; ?>
+
+</div>
